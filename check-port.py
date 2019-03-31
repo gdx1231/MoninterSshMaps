cf957d3c4964c399dff4dd96fe1e4654839368b0
郭磊 <guolei@guoleideMacBook.local>
2019-03-31 17:32:18 +0800

    o

--- check-port.py
+++ check-port.py
@@ -8,33 +8,57 @@ from urllib import urlencode
 from socket import gethostname
 
 
-def check_port():
-    port = [50022]
-    failed_port = []
-    for _each_port in port:
-        try:
-            sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sk.settimeout(2)
-            sk.connect(('ali2.gyap.com.cn', _each_port))
-            # sk.connect(('127.0.0.1', 22))
-            ret_bytes = sk.recv(1024)
-            sk.close
-            ret_str = str(ret_bytes)
-            print(ret_str)
-            now = time.strftime("%Y-%m-%d %H:%M:%S")
-            tt = "check port success!"
-            #logfile = "/home/op/check.log"
-            #f = open(logfile, 'a+')
-            #f.write(now + " " + tt + "\n")
-            #f.close()
-            print now, _each_port, tt
-        except socket.error:
-            failed_port.append(_each_port)
+def check_port(addrAndPort):
+    """
+    检查端口是否可用.
 
-    for _each_port in failed_port:
-        print _each_port
-        
+    Args:
+        addrAndPort: 地址和端口及协议(http/https请求协议是http)，
+        例如 www.baidu.com:80:http
+    Returns:
+       是否连接成功.
+    """
+    addr1 = addrAndPort.split(':')
+    addr = addr1[0]
+    port = int(addr1[1])
+    net_type = ''
+    if len(addr1) > 2:
+        net_type = addr1[2]
+
+    print addr, port, net_type
+    try:
+        sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sk.settimeout(2)
+        sk.connect((addr, port))
+        if port == 80 or port == 443 or net_type == 'http':
+            sk.send('GET / HTTP/1.0\r\n\r\n')
+
+        # sk.connect(('127.0.0.1', 22))
+        ret_bytes = sk.recv(102)
+        sk.close
+        ret_str = str(ret_bytes)
+        print(ret_str)
+        now = time.strftime("%Y-%m-%d %H:%M:%S")
+        tt = "check port success!"
+        #logfile = "/home/op/check.log"
+        #f = open(logfile, 'a+')
+        #f.write(now + " " + tt + "\n")
+        # f.close()
+        print now, port, tt
+        return True
+    except socket.error:
+        print socket.error
+        return False
+
+
+def check_ports(addrs):
+    failed_ports = []
+    for addr in addrs:
+        isok = check_port(addr)
+        if not isok:
+            failed_ports.append(addr)
+    return failed_ports
 
 
 if __name__ == '__main__':
-    check_port()
+    check_ports(['127.0.0.1:22', '192.168.1.252:880:http'])
---
+++ get-net-conn-id.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import psutil
+import sys
+
+def get_listen_pids(checkport):
+    """
+    获取监听端口对应的程序id
+
+    Args:
+        checkport: 检查的端口
+    Returns:
+        监听的程序id数组.
+    """
+    net_ids = []
+    map_ids ={}
+    try:
+        conns = psutil.net_connections()
+        for cnn in conns: 
+            port = cnn.laddr[1]
+            status = cnn.status
+            
+            if status == 'LISTEN' and port == checkport:
+                pid = cnn.pid
+                if not map_ids.has_key(pid):
+                    map_ids[pid] =1
+                    net_ids.append(pid)
+    except psutil.AccessDenied:
+         print "psutil.AccessDenied"
+    
+    return net_ids
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'USAGE: netstats.py checkport'
+        sys.exit(0)
+   
+check_port = int(str(sys.argv[1]))
+# print check_port
+ids = get_listen_pids(check_port)
+print ids
+for id in ids:
+    print id
+
